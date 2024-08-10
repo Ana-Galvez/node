0183d50d8c9874520490ed64db3822cbe1633e5d
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-08-10 01:31:22 -0300

    feat: module and require common js

---
+++ module.js
@@ -0,0 +1,14 @@
+const str= 'hola mundo';
+const arr=[1,2,3,'ana','ezi']
+const obj={
+  name:'ana',
+  lastname:'galvez',
+  age:42
+}
+
+module.exports={
+  str,
+  arr,
+  obj
+}
+
---
+++ require.js
@@ -0,0 +1,4 @@
+const {obj,arr,str}=require('./module.js')
+console.log(obj);
+console.log(arr);
+console.log(str);

ae27dd54ed73e0953f668a780d8124593254468d
Ana Galvez <anamariagalvez01@gmail.com>
2024-08-10 20:31:59 -0300

    refactor: os module

--- os.js
+++ os.js
@@ -2,8 +2,8 @@ const os=require('node:os');
 
 console.log(`Detalles del sistema operativo:
   ${os.version}, ${os.release}, ${os.type}, ${os.platform}, ${os.arch}
-  Total memoria ram: ${os.totalmem}
-  Memoria ram libre: ${os.freemem}
+  Total memoria ram: ${os.totalmem / 1000000000 + ' GB'}
+  Memoria ram libre: ${os.freemem / 1000000000 + ' GB'}
   Cantidad de cpus: ${os.availableParallelism()}
   Ruta de usuario: ${os.homedir}
   Usuario: ${os.hostname}

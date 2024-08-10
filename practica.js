844cbe4eaf8c1f3aa1b3734928112689a2be9d5c
Ana Galvez <anamariagalvez01@gmail.com>
2024-08-09 21:23:49 -0300

    feat: practice process

---
+++ practica.js
@@ -0,0 +1,15 @@
+// console.log(process.version); //version node
+// console.log(process.arch);
+// console.log(process.platform);
+console.log(`nombre de la pc ${process.env.COMPUTERNAME}`);
+console.log(`letra de disco ${process.env.HOMEDRIVE}`);
+console.log(`ruta de la cuenta del usuario ${process.env.HOMEPATH}`);
+console.log(`número de procesadores ${process.env.NUMBER_OF_PROCESSORS}`);
+console.log(`sistema operativo ${process.env.OS}`);
+console.log(`arquitectura del procesador ${process.env.PROCESSOR_ARCHITECTURE}`);
+console.log(`nombre del usuario de la cuenta ${process.env.USERNAME}`);
+
+
+
+
+

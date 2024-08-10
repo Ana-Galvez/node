dd35f8bbdd7eebec844841bf28a05ff1c427db35
Ana Galvez <anamariagalvez01@gmail.com>
2024-08-09 21:04:53 -0300

    feat: process

--- globalObjects.js
+++ globalObjects.js
@@ -1,5 +1,6 @@
-console.log(__dirname); // muestra la ruta del archivo
-console.log(__filename); // muestra la ruta junto con el archivo
-console.log(module);    
-console.log(require); 
+// console.log(__dirname); // muestra la ruta del archivo
+// console.log(__filename); // muestra la ruta junto con el archivo
+// console.log(module);    
+// console.log(require); 
+console.log(process);
 

6424c1af26a35cbfbcac2e534abf3b45db6887c1
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-08-11 00:10:03 -0300

    feat: readFileSync

--- fs.js
+++ fs.js
@@ -1,4 +1,6 @@
 const fs=require('node:fs')
 
-const path=fs.readFileSync('./fs1.txt','utf-8')
-console.log(path);
+const path=fs.readFileSync('./fs1.txt') //devuelve un buffer
+const pathGood=fs.readFileSync('./fs1.txt','utf-8')
+const pathGoodStr=fs.readFileSync('./fs1.txt').toString()
+console.log(pathGoodStr);

c1304d5cb368b595f21ceaacc2bc4e9aa2235d80
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-08-11 00:22:49 -0300

    feat: writeFileSync

--- fs.js
+++ fs.js
@@ -1,6 +1,13 @@
-const fs=require('node:fs')
+const fs = require('node:fs')
 
-const path=fs.readFileSync('./fs1.txt') //devuelve un buffer
-const pathGood=fs.readFileSync('./fs1.txt','utf-8')
-const pathGoodStr=fs.readFileSync('./fs1.txt').toString()
+const path = fs.readFileSync('./fs1.txt') //devuelve un buffer
+const pathGood = fs.readFileSync('./fs1.txt', 'utf-8')
+const pathGoodStr = fs.readFileSync('./fs1.txt').toString()
 console.log(pathGoodStr);
+
+const contain = 'Este contenido se usa para agregarlo a un nuevo archivo o uno ya existente'
+fs.writeFileSync('./fs2.txt', contain);
+
+const addedContain= ' este contenido se agrega a algún archivo que ya existe y con contenido o si no existe lo crea'
+fs.writeFileSync('./fs1.txt',addedContain,{ flag:'a+'}) // hay diferentes flag sea a, r, w con variantes
+
